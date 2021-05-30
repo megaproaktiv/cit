13e11eaff4e1850f720c2d1efe1c525a35fc0a28
Gernot Glawe <gglawe@tecracer.de>
2021-05-30 12:01:48 +0200

    first tests

---
+++ stack.go
@@ -0,0 +1,20 @@
+package cit
+
+type Template struct {
+	AWSTemplateFormatVersion string                 `json:"AWSTemplateFormatVersion,omitempty"`
+	Transform                map[string]interface{} `json:"Transform,omitempty"`
+	Description              string                 `json:"Description,omitempty"`
+	Metadata                 map[string]interface{} `json:"Metadata,omitempty"`
+	Parameters               map[string]interface{} `json:"Parameters,omitempty"`
+	Mappings                 map[string]interface{} `json:"Mappings,omitempty"`
+	Conditions               map[string]interface{} `json:"Conditions,omitempty"`
+	Resources                Resources              `json:"Resources,omitempty"`
+	Rules                    map[string]interface{} `json:"Rules,omitempty"`
+	Outputs                  map[string]interface{} `json:"Outputs,omitempty"`
+}
+
+type Resources map[string]Resource
+
+type Resource struct {
+	Metadata map[string]interface{} `json:"Metadata,omitempty"`
+}
