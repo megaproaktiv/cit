53f31025d683aa5f2671433215ae29faf6556749
Gernot Glawe <gglawe@tecracer.de>
2021-05-30 12:01:48 +0200

    first tests

--- getphysical.go
+++ getphysical.go
@@ -1,13 +1,44 @@
 package cit
 
 import (
-	aws "github.com/aws/aws-sdk-go-v2/aws"
+	"fmt"
+	"log"
+	"strings"
 
+	aws "github.com/aws/aws-sdk-go-v2/aws"
+	goformation "github.com/awslabs/goformation/v4"
+	"github.com/awslabs/goformation/v4/intrinsics"
 )
 
-// PhysicalID get AWS physical id from CDK contruct ID
+// PhysicalIDfromCID get AWS physical id from CDK contruct ID
 // stack - Stackname
 // constructID -  awssns.NewTopic(stack, jsii.String("MyTopic")  << MyTopic
-func PhysicalID(stack *string, constructId *string) *string {
+func PhysicalIDfromCID(stack *string, constructId *string) *string {
 	return aws.String("not implemented")
+}
+
+// LogicalIDfromCID - get logicalID
+func LogicalIDfromCID(stackfile *string, constructID *string) *string{
+
+	// Do not process, goformation can`t handle cdk generated data
+	template, err := goformation.OpenWithOptions(*stackfile, &intrinsics.ProcessorOptions{
+		NoProcess: true,
+		EvaluateConditions: false,
+	})
+	if err != nil {
+		log.Fatalf("There was an error processing the template: %s", err)
+	}
+
+	for _, resource := range template.Resources {
+		fmt.Println(resource.AWSCloudFormationType())
+	}
+
+	
+
+	return nil
+}
+
+func ExtractLogicalName( path *string) string{
+	var parts = strings.Split(*path, "/")
+	return parts[1]
 }
--- getphysical_test.go
+++ getphysical_test.go
@@ -1,14 +1,16 @@
 package cit_test
 
 import (
-	"github.com/megaproaktiv/cit"
 	"context"
+	"encoding/json"
+	"io/ioutil"
 	"testing"
-	"github.com/stretchr/testify/assert"
 
 	aws "github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/megaproaktiv/cit"
+	"github.com/stretchr/testify/assert"
 )
 
 // Deploy testData stack before
@@ -26,10 +28,52 @@ func TestPhysicalID(t *testing.T) {
 	}
 
 	res, err := client.GetParameter(context.TODO(), params)
+	if err != nil {
+		panic("Cant get SSM parameter - deploy testdata/cdksns stack ")
+	}
 	ssmid := res.Parameter.Value
 
-	pID := cit.PhysicalID(aws.String("CdksnsStack"),aws.String("MyTopic"))
+	pID := cit.PhysicalIDfromCID(aws.String("CdksnsStack"), aws.String("MyTopic"))
 
 	assert.Equal(t, *ssmid, *pID, "PhysicalID should match ConstructID")
+
+}
+
+func TestLogicalID(t *testing.T) {
+	testfile := "testdata/template.json"
+
+	logicalID := cit.LogicalIDfromCID(&testfile, aws.String("MyTopic"))
+
+	assert.Equal(t, "MyTopic86869434", *logicalID)
+
+}
+
+func TestMarshallTemplate(t *testing.T){
+	testfile := "testdata/template-raw.json"
+
+	stack := &cit.Template{}
+
+	data, err := ioutil.ReadFile(testfile)
 	
+	if err != nil {
+		panic("Cannot read "+testfile)
+	}
+
+	err = json.Unmarshal(data, stack); 
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Nil(t, err, "Unmarshal should work wo errors")
+
+	logicalName := "MyTopic86869434"
+	for key := range stack.Resources { 
+		assert.Equal(t, logicalName, key)	
+	}
+	resource := stack.Resources[logicalName]
+	assert.Equal(t, resource.Metadata["aws:cdk:path"], "CdksnsStack/MyTopic/Resource")
+}
+
+func TestExtractLogicalName(t *testing.T){
+	path := "CdksnsStack/MyTopic/Resource"
+	assert.Equal(t, "MyTopic", cit.ExtractLogicalName(&path))
 }
