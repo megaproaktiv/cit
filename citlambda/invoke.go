4bb15adf6fc2e61809b9520f6633f29febaa8e19
Gernot Glawe <gglawe@tecracer.de>
2021-06-26 12:28:13 +0200

    get invoke from main
    - Cahnges from gitpod and from local

--- citlambda/invoke.go
+++ citlambda/invoke.go
@@ -38,4 +38,8 @@ func InvokeFunction(stackname *string, constructID *string, eventFile *string)( 
 	}
 	responseString := string(res.Payload)
 	return &responseString, nil
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 87e5abe (get nvoke from main)
