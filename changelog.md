f7cec94353b0092dcb35b83dd752a8d5e4c827a3
Gernot Glawe <gglawe@tecracer.de>
2021-12-12 18:31:28 +0100

    Bugfix Stack

--- changelog.md
+++ changelog.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Added
 
+## [0.1.13]
+### Bugfix
+- Error json: cannot unmarshal bool into Go struct field Resource.Resources.Metadata of type string
+
 ## [0.1.12]
 ### Added
 - Lambda with mocked Cfn and lambda client
--- stack.go
+++ stack.go
@@ -1,20 +1,28 @@
 package cit
 
+// Template Content
+// See
+// https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-anatomy.html
+// and
+// https://github.com/awslabs/goformation/blob/d7193b5126b26a12ad1c810c7c481023cef849d8/cloudformation/template.go#L14
 type Template struct {
 	AWSTemplateFormatVersion string                 `json:"AWSTemplateFormatVersion,omitempty"`
-	Transform                map[string]interface{} `json:"Transform,omitempty"`
 	Description              string                 `json:"Description,omitempty"`
 	Metadata                 map[string]interface{} `json:"Metadata,omitempty"`
 	Parameters               map[string]interface{} `json:"Parameters,omitempty"`
-	Mappings                 map[string]interface{} `json:"Mappings,omitempty"`
-	Conditions               map[string]interface{} `json:"Conditions,omitempty"`
-	Resources                Resources              `json:"Resources,omitempty"`
 	Rules                    map[string]interface{} `json:"Rules,omitempty"`
+	Mappings                 map[string]interface{} `json:"Mappings,omitempty"`
+	CDKMetadata              map[string]interface{} `json:"CDKMetadata,omitempty"`
+	Conditions               map[string]interface{} `json:"Conditions,omitempty"`
+	Transform                map[string]interface{} `json:"Transform,omitempty"`
+	Resources                Resources              `json:"Resources,omitempty"`
 	Outputs                  map[string]interface{} `json:"Outputs,omitempty"`
+
+
 }
 
 type Resources map[string]Resource
 
 type Resource struct {
-	Metadata map[string]string `json:"Metadata,omitempty"`
+	Metadata map[string]interface{} `json:"Metadata,omitempty"`
 }
